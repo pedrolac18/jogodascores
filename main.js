f38d5a0e431ca6afbf6f441a34723964081baa29
Pedro Lacerda <83882322+pedrolac18@users.noreply.github.com>
2021-08-18 14:22:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+var chancecolor = document.getElementById('display-cores');
+
+
+
+function amarelo() {
+ chancecolor.style.background = "#ffff00";
+
+}
+
+function verde() {
+  chancecolor.style.background = "#005221";
+
+}
+
+function azul(){
+  chancecolor.style.background = "#26a5cb";
+
+}
+function vermelho(){
+  chancecolor.style.background = "#cc0000";
+
+}
+function laranja(){
+  chancecolor.style.background = "#ff8c00";
+
+}
+function marrom(){
+  chancecolor.style.background = "#8b4513";
+
+}
+function rosa(){
+  chancecolor.style.background = "#fc0fc0";
+
+}
+function roxo(){
+  chancecolor.style.background = "#5a005a";
+
+}
+function cinza(){
+  chancecolor.style.background = "#646c64";
+
+}
+function preto(){
+  chancecolor.style.background = "#010101";
+
+}
+
+
+
+
+
+function dois(){
+  chancecolor.style.opacity = "20%";
+}
+function quatro(){
+  chancecolor.style.opacity = "40%";
+}
+function seis(){
+  chancecolor.style.opacity = "60%";
+}
+function oito(){
+  chancecolor.style.opacity = "80%";
+}
+function dez(){
+  chancecolor.style.opacity = "100%";
+}
